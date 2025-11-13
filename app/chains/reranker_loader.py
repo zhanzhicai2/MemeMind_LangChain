1b0a1c696dd323f7816579abb7c2fdfb3e65cebd
zhanzhicai2 <1610893869@qq.com>
2025-11-13 11:35:55 +0800

    测试

---
+++ app/chains/reranker_loader.py
@@ -0,0 +1,11 @@
+# -*- coding: UTF-8 -*-
+"""
+@File ：reranker_loader.py
+@IDE ：PyCharm
+@Author ：zhanzhicai
+@Date ：2025/11/13 11:34
+@DOC: 
+"""
+from MemeMind_LangChain.app.core.reranker_qwen import RERANKER_MODEL_PATH
+
+print("Reranker 模型路径:", RERANKER_MODEL_PATH)
